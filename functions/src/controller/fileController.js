b45aec8582cae37e3f3c576842bb76a6b340049a
Nguyen <hai.b.nguyen@accenturefederal.com>
2020-01-11 11:14:14 -0600

    upload file firebase

--- functions/src/controller/fileController.js
+++ functions/src/controller/fileController.js
@@ -1,11 +1,56 @@
 (function () {
     var router = require('express').Router(),
-        request = require('request'),
-        middleware = require('../../util/middleware');
+        middleware = require('../../util/middleware'),
+        os = require('os'),
+        path = require('path'),
+        Busboy = require('busboy'),
+        fs = require('fs'),
+        UUID = require("uuid-v4");
 
-    router.post('/svc/uploadFile', middleware.isValidUser, (req, res) => {
-        var newUrl = 'https://us-central1-architect-c592d.cloudfunctions.net/uploadFile';
-        req.pipe(request(newUrl)).pipe(res);
-    });
+    const functions = require('firebase-functions');
+    const gcconfig = {
+        projectId: 'architect-c592d',
+        keyFilename: 'architect-c592d-firebase-adminsdk-wocm3-b6d41a751d.json'
+    }
+    const { Storage } = require('@google-cloud/storage');
+    const gcs = new Storage(gcconfig);
+
+    router.post('/svc/uploadFile', middleware.isValidUser, functions.https.onRequest((req, res) => {
+        if (req.method !== "POST") {
+            return res.status(500).json({ message: "Not allowed" });
+        }
+        const busboy = new Busboy({ headers: req.headers });
+
+        let uploadData = null;
+        let uuid = UUID();
+
+        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+            const filepath = path.join(os.tmpdir(), uuid + filename);
+            uploadData = { file: filepath, type: mimetype };
+            file.pipe(fs.createWriteStream(filepath));
+        });
+
+        busboy.on('finish', () => {
+            const bucket = gcs.bucket('architect-c592d.appspot.com');
+            bucket.upload(uploadData.file, {
+                uploadType: 'media',
+                metadata: {
+                    metadata: {
+                        contentType: uploadData.type,
+                        firebaseStorageDownloadTokens: uuid
+                    }
+                }
+            }).then((data) => {
+                let file = data[0];
+                return res.status(200).json({ fileLocation: "https://firebasestorage.googleapis.com/v0/b/" + bucket.name + "/o/" + encodeURIComponent(file.name) + "?alt=media&token=" + uuid });
+            }).catch((err) => {
+                res.status(500).json({
+                    error: err
+                });
+            });
+        });
+
+        return req.pipe(busboy);
+    }));
     module.exports = router;
 }());
