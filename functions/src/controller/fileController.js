b14514c36036e042a0581650214ccaf9067f17e2
Nguyen <hai.b.nguyen@accenturefederal.com>
2020-02-03 16:32:24 -0600

    upload svc

--- functions/src/controller/fileController.js
+++ functions/src/controller/fileController.js
@@ -1,108 +1,12 @@
 (function () {
     const router = require('express').Router(),
         middleware = require('../../util/middleware'),
-        os = require('os'),
-        path = require('path'),
-        Busboy = require('busboy'),
-        fs = require('fs'),
-        UUID = require("uuid-v4"),
-        getRawBody = require('raw-body'),
-        contentType = require('content-type'),
         functions = require('firebase-functions'),
-        { Storage } = require('@google-cloud/storage');
+        fileSvc = require('../services/fileSvc');
 
-    let uuid = UUID();
-
-    router.post('/svc/uploadFile', [middleware.isValidUser, middleF1, middleF2], functions.https.onRequest((req, res) => {
+    router.post('/svc/uploadFile', [middleware.isValidUser, fileSvc.middleF1, fileSvc.middleF2], functions.https.onRequest((req, res) => {
 
     }));
-
-    function middleF1(req, res, next) {
-        if (
-            req.rawBody === undefined &&
-            req.method === 'POST' &&
-            req.headers['content-type'].startsWith('multipart/form-data')
-        ) {
-            getRawBody(
-                req,
-                {
-                    length: req.headers['content-length'],
-                    limit: '10mb',
-                    encoding: contentType.parse(req).parameters.charset,
-                },
-                (err, string) => {
-                    if (err) return next(err);
-                    req.rawBody = string;
-                    return next();
-                }
-            );
-        }
-        return next();
-    }
-
-    function middleF2(req, res, next) {
-        if (
-            req.method === 'POST' &&
-            req.headers['content-type'].startsWith('multipart/form-data')
-        ) {
-            const gcconfig = {
-                projectId: 'm2meme',
-                keyFilename: 'm2meme-firebase-adminsdk-rvmhz-dff76c1bfa.json'
-            },
-                gcs = new Storage(gcconfig),
-                busboy = new Busboy({
-                    headers: req.headers,
-                });
-
-            var fileBuffer = new Buffer('');
-            req.data = {};
-            busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-                file.on('data', data => {
-                    fileBuffer = Buffer.concat([fileBuffer, data])
-                });
-
-                file.on('end', () => {
-                    const file_object = {
-                        fieldname,
-                        originalname: filename,
-                        encoding,
-                        mimetype,
-                        buffer: fileBuffer,
-                    };
-                    req.file = file_object;
-                });
-                const filepath = path.join(os.tmpdir(), uuid + filename);
-                req.data['file'] = filepath;
-                file.pipe(fs.createWriteStream(filepath));
-            });
-
-            busboy.on('field', (fieldname, val) => {
-                req.data[fieldname] = val;
-            });
-
-            busboy.on('finish', () => {
-                const bucket = gcs.bucket('m2meme.appspot.com');
-                bucket.upload(req.data.file, {
-                    uploadType: 'media',
-                    metadata: {
-                        metadata: {
-                            firebaseStorageDownloadTokens: uuid
-                        }
-                    }
-                }).then((data) => {
-                    let file = data[0];
-                    fs.unlinkSync(req.data.file);
-                    return res.status(200).json({ fileLocation: "https://firebasestorage.googleapis.com/v0/b/" + bucket.name + "/o/" + encodeURIComponent(file.name) + "?alt=media&token=" + uuid });
-                }).catch((err) => {
-                    console.log(err);
-                    return res.status(500).json(err);
-                });
-            });
-            busboy.end(req.rawBody);
-            req.pipe(busboy);
-        }
-        return next();
-    }
 
     module.exports = router;
 }());
---
+++ functions/src/services/fileSvc.js
@@ -0,0 +1,101 @@
+(function () {
+    const os = require('os'),
+        path = require('path'),
+        Busboy = require('busboy'),
+        fs = require('fs'),
+        UUID = require("uuid-v4"),
+        getRawBody = require('raw-body'),
+        contentType = require('content-type'),
+        { Storage } = require('@google-cloud/storage');
+
+    module.exports = {
+        middleF1: middleF1,
+        middleF2: middleF2
+    };
+
+    function middleF1(req, res, next) {
+        if (req.rawBody === undefined &&
+            req.method === 'POST' &&
+            req.headers['content-type'].startsWith('multipart/form-data')) {
+            getRawBody(
+                req,
+                {
+                    length: req.headers['content-length'],
+                    limit: '10mb',
+                    encoding: contentType.parse(req).parameters.charset,
+                },
+                (err, string) => {
+                    if (err) return next(err);
+                    req.rawBody = string;
+                    return next();
+                    //next();
+                }
+            );
+        }
+        return next();
+        //next();
+    }
+
+    function middleF2(req, res, next) {
+        if (req.method === 'POST' &&
+            req.headers['content-type'].startsWith('multipart/form-data')) {
+            let uuid = UUID();
+            const gcconfig = {
+                projectId: 'm2meme',
+                keyFilename: 'm2meme-firebase-adminsdk-rvmhz-dff76c1bfa.json'
+            },
+                gcs = new Storage(gcconfig),
+                busboy = new Busboy({
+                    headers: req.headers,
+                });
+
+            var fileBuffer = new Buffer('');
+            req.data = {};
+            busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+                file.on('data', data => {
+                    fileBuffer = Buffer.concat([fileBuffer, data])
+                });
+
+                file.on('end', () => {
+                    const file_object = {
+                        fieldname,
+                        originalname: filename,
+                        encoding,
+                        mimetype,
+                        buffer: fileBuffer,
+                    };
+                    req.file = file_object;
+                });
+                const filepath = path.join(os.tmpdir(), uuid + path.extname(filename));
+                req.data['file'] = filepath;
+                file.pipe(fs.createWriteStream(filepath));
+            });
+
+            busboy.on('field', (fieldname, val) => {
+                req.data[fieldname] = val;
+            });
+
+            busboy.on('finish', () => {
+                const bucket = gcs.bucket('m2meme.appspot.com');
+                bucket.upload(req.data.file, {
+                    uploadType: 'media',
+                    metadata: {
+                        metadata: {
+                            firebaseStorageDownloadTokens: uuid
+                        }
+                    }
+                }).then((data) => {
+                    let file = data[0];
+                    fs.unlinkSync(req.data.file);
+                    return res.status(200).json({ fileLocation: "https://firebasestorage.googleapis.com/v0/b/" + bucket.name + "/o/" + encodeURIComponent(file.name) + "?alt=media&token=" + uuid });
+                }).catch((err) => {
+                    console.log(err);
+                    return res.status(500).json(err);
+                });
+            });
+            busboy.end(req.rawBody);
+            req.pipe(busboy);
+        }
+        return next();
+    }
+}());
