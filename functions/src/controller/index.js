eff2d908fe2bb727c6d0c7d65dbbe051fe270e07
Nguyen <hai.b.nguyen@accenturefederal.com>
2020-03-22 18:08:02 -0500

    temp

--- functions/src/controller/index.js
+++ functions/src/controller/index.js
@@ -3,8 +3,14 @@
         router = express.Router();
 
     // Define the home page route
-    router.get('/', (req, res) => {
-        res.send(req.user);
+    router.get('/:id', (req, res) => {
+        res.send('<!DOCTYPE html><head>' +
+            '<title>' + ' | Me2meme Website</title>' +
+            '<meta property="og:title" content="' + '">' +
+            '<meta property="twitter:title" content="' + '">' +
+            '<link rel="icon" href="https://m2meme.firebaseapp.com/assets/image/logo256x215.png">' +
+            '</head><body>' + 'Hello world ' + req.params.id +
+            '</body></html>');
     });
 
     module.exports = router;
