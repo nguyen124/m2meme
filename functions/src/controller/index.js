0bc42aabbcf72e4a23ad29c53732e2dd76366aff
Nguyen <hai.b.nguyen@accenturefederal.com>
2020-03-22 23:57:27 -0500

    need to solve picture problem more

--- functions/src/controller/index.js
+++ functions/src/controller/index.js
@@ -4,14 +4,15 @@
 
     // Define the home page route
     router.get('/', (req, res) => {
-        var html = '<!DOCTYPE html><head>' +
+        var html = '<html><head>' +
             '<meta property="og:title" content="' + req.query.title + '">' +
             '<meta property="og:image" content="' + req.query.image + '">' +
+            '<meta property="og:image:width" content="' + 600 + '">' +
+            '<meta property="og:image:height" content="' + 314 + '">' +
             '<meta property="og:description" content="' + req.query.description + '">' +
             '<meta property="og:url" content="' + req.query.url + '">' +
-            '</head><body>' + 'Hello world ' + req.query.title +
+            '</head><body>' + '<script>window.location="https://m2meme.firebaseapp.com/?id=' + req.query.id + '";</script>' +
             '</body></html>';
-        console.log(html);
         res.send(html);
     });
 
