594796e816b71f0e0d13014e3d56819b3bc6b7b9
Hai Nguyen <nguyenhohai88@gmail.com>
2020-03-23 23:48:53 -0500

    url share

--- functions/src/controller/index.js
+++ functions/src/controller/index.js
@@ -11,7 +11,7 @@
             '<meta property="og:image:height" content="' + 314 + '">' +
             '<meta property="og:description" content="' + req.query.description + '">' +
             '<meta property="og:url" content="' + req.query.url + '">' +
-            '</head><body>' + '<script>window.location="https://m2meme.firebaseapp.com/?id=' + req.query.id + '";</script>' +
+            '</head><body>' + '<script>window.location="https://me2meme.com/?id=' + req.query.id + '";</script>' +
             '</body></html>';
         res.send(html);
     });
