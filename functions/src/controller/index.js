45895cc3a3de70a474dd1373f2964d1d5a13063c
Hai Nguyen <nguyenhohai88@gmail.com>
2020-04-22 16:58:01 -0500

    twitter card

--- functions/src/controller/index.js
+++ functions/src/controller/index.js
@@ -5,17 +5,17 @@
     // Define the home page route
     router.get('/', (req, res) => {
         var html = '<html><head>' +
-            '<meta property="og:title" content="' + req.query.title + '">' +            
-            '<meta property="og:image" content="' + req.query.image + '">' +            
+            '<meta property="og:title" content="' + req.query.title + '">' +
+            '<meta property="og:image" content="' + req.query.image + '">' +
             '<meta property="og:image:width" content="' + 600 + '">' +
             '<meta property="og:image:height" content="' + 314 + '">' +
             '<meta property="og:description" content="' + req.query.description + '">' +
             '<meta property="og:url" content="' + req.query.url + '">' +
-            '<meta property="twitter:card" content="' + req.query.title + '">' +
-            '<meta property="twitter:image" content="' + req.query.image + '">' +
-            '<meta property="twitter:title" content="' + req.query.title + '">' +
-            '<meta property="twitter:description" content="' + req.query.description + '">' +
-            '</head><body>' + '<script>window.location="https://me2meme.com/?id=' + req.query.id + '";</script>' +
+            '<meta name="twitter:card" content="' + req.query.title + '">' +
+            '<meta name="twitter:image" content="' + req.query.image + '">' +
+            '<meta name="twitter:title" content="' + req.query.title + '">' +
+            '<meta name="twitter:description" content="' + req.query.description + '">' +
+            '</head><body>' + '<script>window.location="https://me2meme.com/?id=' + req.query.id + '"</script>' +
             '</body></html>';
         res.send(html);
     });
