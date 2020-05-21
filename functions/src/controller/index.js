2bcb9f6687d34ab968e12470bd3908e4e6ff8991
Hai Nguyen <nguyenhohai88@gmail.com>
2020-05-21 15:21:53 -0500

    share on fb

--- functions/src/controller/index.js
+++ functions/src/controller/index.js
@@ -15,7 +15,31 @@
             '<meta name="twitter:image" content="' + req.query.image + '">' +
             '<meta name="twitter:title" content="' + req.query.title + '">' +
             '<meta name="twitter:description" content="' + req.query.description + '">' +
-            '</head><body>' + '<script>window.location="https://me2meme.com/?id=' + req.query.id + '"</script>' +
+            '</head><body>' + '<script>window.location="' + req.query.url + '"</script>' +
+            '</body></html>';
+        res.send(html);
+    });
+
+    router.get('/share/video', (req, res) => {
+        var html = '<html><head>' +
+            '<meta property="og:title" content="' + req.query.title + '">' +
+            '<meta property="og:video" content="' + req.query.video + '">' +
+            '<meta property="og:video:secure_url" content="' + req.query.video + '">' +
+            '<meta property="og:video:type" content="video/mp4">' +
+            '<meta property="og:video:width" content="' + 600 + '">' +
+            '<meta property="og:video:height" content="' + 314 + '">' +
+            '<meta property="og:description" content="' + req.query.description + '">' +
+            '<meta property="og:image" content="https://me2meme.com/assets/image/logo256x215.png">' +
+            '<meta property="og:url" content="' + req.query.url + '">' +
+            '<meta name="twitter:card" content="player">' +
+            '<meta name="twitter:title" content="' + req.query.title + '">' +
+            '<meta name="twitter:site" content="MasterHai">' +
+            '<meta name="twitter:description" content="' + req.query.description + '">' +
+            '<meta name="twitter:player" content="' + req.query.video + '">' +
+            '<meta name="twitter:player:width" content="' + 600 + '">' +
+            '<meta name="twitter:player:height" content="' + 314 + '">' +
+            '<meta name="twitter:image" content="https://me2meme.com/assets/image/logo256x215.png">' +
+            '</head><body>' + '<script>window.location="' + req.query.url + '"</script>' +
             '</body></html>';
         res.send(html);
     });
