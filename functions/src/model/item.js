550743a18169f51936a186f348c687e2fe21950a
Hai Nguyen <nguyenhohai88@gmail.com>
2022-07-31 23:11:38 -0500

    Nail Salon

--- functions/src/model/item.js
+++ functions/src/model/item.js
@@ -1,39 +1,62 @@
 (function () {
-    var mongoose = require('mongoose'),
-        itemSchema = mongoose.Schema({
-            title: {
-                type: String,
-                maxlength: 150
-            },
-            files: [Object],
-            modifiedDate: Date,
-            createdBy: Object,
-            tags: {
-                type: [{
-                    type: String,
-                    maxlength: 30
-                }],
-                validate: [arrayLimit, "{PATH} exceeds the limit of 5"]
-            },
-            categories: [String],
-            noOfPoints: Number,
-            noOfComments: Number,
-            noOfViews: Number,
-            hasUpvoted: Boolean,
-            hasDownvoted: Boolean,
-            hasReported: Boolean,
-            description: {
-                type: String,
-                maxlength: 2000
-            },
-            overview: {
-                type: String,
-                maxlength: 500
-            }
-        });
-    function arrayLimit(val) {
-        return val.length <= 5;
-    }
-    itemSchema.index({ modifiedDate: -1, tags: 1 });
-    module.exports = mongoose.model('item', itemSchema);
-}());
+  var mongoose = require("mongoose"),
+    itemSchema = mongoose.Schema({
+      title: {
+        type: String,
+        maxlength: 150,
+      },
+      businessName: {
+        type: String,
+        maxlength: 150,
+      },
+      files: [Object],
+      modifiedDate: Date,
+      createdBy: Object,
+      tags: {
+        type: [
+          {
+            type: String,
+            maxlength: 30,
+          },
+        ],
+        validate: [arrayLimit, "{PATH} exceeds the limit of 5"],
+      },
+      categories: [String],
+      noOfPoints: Number,
+      noOfComments: Number,
+      noOfViews: Number,
+      hasUpvoted: Boolean,
+      hasDownvoted: Boolean,
+      hasReported: Boolean,
+      price: Number,
+      address: String,
+      zipcode: String,
+      city: String,
+      state: String,
+      country: String,
+      noOfEmployees: Number,
+      noOfChairs: Number,
+      noOfTables: Number,
+      contactPhoneNo: String,
+      contactEmail: String,
+      income: Number,
+      rentCost: Number,
+      otherCost: Number,
+      leaseEnd: Date,
+      yearOld: Number,
+      area: Number,
+      description: {
+        type: String,
+        maxlength: 2000,
+      },
+      overview: {
+        type: String,
+        maxlength: 500,
+      },
+    });
+  function arrayLimit(val) {
+    return val.length <= 5;
+  }
+  itemSchema.index({ modifiedDate: -1, tags: 1 });
+  module.exports = mongoose.model("item", itemSchema);
+})();
